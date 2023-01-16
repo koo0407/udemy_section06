32c40d83ae7a408078042f7ae129d3ade8e81af5
Koo Kyeongyeong <koo0407@gmail.com>
2023-01-16 21:32:49 +0900

    084. 동적 인라인 스타일 설정하기
    불리언 값으로 입력된 값의 길이를 확인하여 state로 관리
    style 속성에 삼항 연산자와 불리언state를 이용하여 적용

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState(""); //입력 값
+  const [isValid, setIsValid] = useState(true);
+  
+  //모든 키의 입력이 있을 때 반응하는 핸들러
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    //입력된 값이 없을 때(길이=0) ->값 전달 x
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+    //입력된 값이 있을 때(길이>0) -> 입력 값 전달
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* 동적 인라인 스타일 */}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            backgroundColor: !isValid ? "salmon" : "transparent",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
